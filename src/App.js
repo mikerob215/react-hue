0c5d8526b4f58e2427546746541f6a8a5acc6c81
mikerobinson <mikerobinson1@gmail.com>
2017-08-18 22:42:31 -0400

    Adds some packages

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+import Home from "./views/home/home";
+import AppBar from "react-toolbox/lib/app_bar/AppBar";
 
 class App extends Component {
     render() {
         return (
-            <div className="App">
-                <div className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <h2>Welcome to React</h2>
-                </div>
-                <p className="App-intro">
-                    To get started, edit <code>src/App.js</code> and save to reload.
-                </p>
+            <div>
+                <AppBar>
+                    adsf
+                </AppBar>
+                <BrowserRouter>
+                    <Switch>
+                        <Route path="/" exact component={Home}/>
+                    </Switch>
+                </BrowserRouter>
             </div>
         );
     }
