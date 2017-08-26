f58be7a497c9a9ada2dcd2f70d1fc8b753aa85fd
mikerobinson <mikerobinson1@gmail.com>
2017-08-26 16:43:39 -0400

    Refactors hubsReducer to use createReducer helper

--- src/reducers/hubs-reducer.js
+++ src/reducers/hubs-reducer.js
@@ -1,26 +1,24 @@
-import {HUB_FETCH_SUCCESSFUL, HUBS_FETCHED} from '../actions/hubs-actions';
 import * as R from 'ramda';
+import createReducer from "../lib/redux-helpers/create-reducer";
 
 const initialState = {
   hubs: [],
   status: 'INITIAL',
 };
 
-const hubsReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case HUBS_FETCHED:
-      return {
-        ...state,
-        status: 'LOADING',
-      };
-    case HUB_FETCH_SUCCESSFUL:
-      return {
-        status: 'SUCCESSFUL',
-        hubs: R.compose(R.map(R.head), R.groupBy(R.prop('id')))(action.payload),
-      };
-    default:
-      return state;
-  }
-};
+const hubsReducer = createReducer(initialState, {
+    HUBS_FETCHED(state) {
+        return {
+            ...state,
+            status: 'LOADING',
+        };
+    },
+    HUB_FETCH_SUCCESSFUL(state, action) {
+        return {
+            status: 'SUCCESSFUL',
+            hubs: R.compose(R.map(R.head), R.groupBy(R.prop('id')))(action.payload),
+        };
+    }
+});
 
 export default hubsReducer;
