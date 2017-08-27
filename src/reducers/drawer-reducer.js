33cd008c16476ea3542168bba76ab7387bf39493
mikerobinson <mikerobinson1@gmail.com>
2017-08-27 17:20:42 -0400

    More ramdaish drawerReducer

--- src/reducers/drawer-reducer.js
+++ src/reducers/drawer-reducer.js
@@ -3,9 +3,7 @@ import * as R from "ramda";
 import {DRAWER_STATE_CHANGED} from "../actions/drawer-actions";
 
 const drawerReducer = createReducer(false, {
-  [DRAWER_STATE_CHANGED](state) {
-    return R.not(state);
-  },
+  [DRAWER_STATE_CHANGED]: R.not,
 });
 
 export default drawerReducer;
